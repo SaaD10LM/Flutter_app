92ad0023f2b9f53af19e40e7d34572cf8ac64c37
SaaD10LM <116724211+SaaD10LM@users.noreply.github.com>
2025-05-24 09:46:48 +0100

    Update custom.widget.drawer.dart

--- app/lib/widgets/custom.widget.drawer.dart
+++ app/lib/widgets/custom.widget.drawer.dart
@@ -1,44 +1,50 @@
-import 'package:app/widgets/drawer.item.dart';
 import 'package:flutter/material.dart';
+import 'package:app/widgets/drawer.item.dart';
+import 'package:app/widgets/header.drawer.dart';
 
-class MyCutsomDrawer extends StatelessWidget {
-  const MyCutsomDrawer({super.key});
+class MyCustomDrawer extends StatelessWidget {
+  const MyCustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
-          MyCutsomDrawer(),
+          const MyHeaderDrawer(),
           MyDrawerItem(
             title: "Home",
-            itemIcon: Icon(Icons.home,
-            color: Theme.of(context).primaryColor),
+            itemIcon: Icon(
+              Icons.home,
+              color: Theme.of(context).primaryColor,
+            ),
             route: "/",
           ),
-          Divider(height: 1, color: Theme
-              .of(context)
-              .primaryColor),
-        MyDrawerItem(
-            title: "Counter",
-            itemIcon: Icon(Icons.ac_unit,
+          Divider(
+            height: 1,
             color: Theme.of(context).primaryColor,
+          ),
+          MyDrawerItem(
+            title: "Counter",
+            itemIcon: Icon(
+              Icons.ac_unit,
+              color: Theme.of(context).primaryColor,
             ),
-           route: "/counter",
-        ),
-          Divider(height: 1, color: Theme
-              .of(context)
-              .primaryColor),
-         MyDrawerItem(
-             title: "Chat",
-             itemIcon: Icon(Icons.access_alarm,
-               color: Theme.of(context).primaryColor ,
-             ),
-             route: "/chat",
-         ),
+            route: "/counter",
+          ),
+          Divider(
+            height: 1,
+            color: Theme.of(context).primaryColor,
+          ),
+          MyDrawerItem(
+            title: "Chat",
+            itemIcon: Icon(
+              Icons.access_alarm,
+              color: Theme.of(context).primaryColor,
+            ),
+            route: "/chat",
+          ),
         ],
       ),
     );
-
   }
 }
